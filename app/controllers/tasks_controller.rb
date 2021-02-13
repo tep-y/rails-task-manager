2b49da663835d3ddcc55c58098673283dd91b91a
tep yith <tep.yith@gmail.com>
2021-02-13 17:00:10 -0500

    Progress

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,6 +18,22 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
